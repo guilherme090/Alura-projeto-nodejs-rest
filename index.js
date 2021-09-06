1970408aa0656702325505ccd0c6aa6eb294e476
Guilherme <guilherme-elec12@hotmail.com>
2021-09-05 22:52:42 -0300

    adicionado banco de dados

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro);
+    } else{
+        console.log('Banco de dados conectado com sucesso.');
+
+        Tabelas.init(conexao);
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000.'));
+    }
+});
 const app = customExpress();
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000.'));
 
 app.get('/', (req, res) => res.send('Servidor rodando, tudo ok!'));
