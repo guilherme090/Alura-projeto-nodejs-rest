d9eeea06b1a155cf42ee2fc2d2a7aa7545bfe36c
Guilherme <guilherme-elec12@hotmail.com>
2021-09-10 18:32:14 -0300

    utilização de HTTP Statuses

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,8 +4,7 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET.'));
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
-        Atendimento.adiciona(atendimento);
-        res.send('Post atendimento.');
+        Atendimento.adiciona(atendimento, res);
     });
 }
 
