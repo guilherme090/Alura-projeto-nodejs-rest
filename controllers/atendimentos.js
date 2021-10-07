1862f00edf1c21f02da9b944d9617f8467199745
Guilherme <guilherme-elec12@hotmail.com>
2021-10-07 18:02:16 -0300

    métodos HTTP: PUT/PATCH

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -5,14 +5,23 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         Atendimento.lista(res);
     });
+
     app.get('/atendimentos/:id', (req,res) => {
         const id = parseInt(req.params.id);
 
         atendimentos.buscaPorId(id, res);
     });
+
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
         Atendimento.adiciona(atendimento, res);
+    });
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        Atendimento.altera(id, valores, res);
     });
 }
 
